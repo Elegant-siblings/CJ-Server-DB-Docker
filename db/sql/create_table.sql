08c50dd6684a5c52bdd81da5a49e6d9e52d0e541
asd147asd147 <asd147asd147@naver.com>
2022-07-26 15:27:31 +0900

    Add Map pos

--- db/sql/create_table.sql
+++ db/sql/create_table.sql
@@ -4,35 +4,15 @@ CREATE TABLE deliveryInfo(
        deliveryTime VARCHAR(10),
        deliveryMan INT,
        sender VARCHAR(100),
-       senderAddress VARCHAR(60),
        reciver VARCHAR(100),
-       reciverAddress VARCHAR(60),
-       deliveryCategory VARCHAR(10)
+       itemCategory VARCHAR(10),
+       senderAddr1 VARCHAR(60),
+       senderAddr2 VARCHAR(60),
+       senderAddr3 VARCHAR(60),
+       reciverAddr1 VARCHAR(60),
+       reciverAddr2 VARCHAR(60),
+       reciverAddr3 VARCHAR(60)
 );
 LOAD DATA INFILE '/var/lib/mysql-files/cj_delivery.csv' INTO TABLE deliveryInfo FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
 flush privileges;
-
-
--- CREATE TABLE Student(
---        student_id INT PRIMARY KEY AUTO_INCREMENT, 
---        student_name VARCHAR(60), 
---        student_age INT
--- );
--- INSERT INTO Student(
---        student_name, 
---        student_age) 
--- VALUES(
---        "Shubham verma", 
---         21
--- );
--- INSERT INTO Student(
---        student_name, 
---        student_age) 
--- VALUES(
---        "Utkarsh verma", 
---         23
--- );
--- ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password'; 
--- flush privileges;
-
--- server/app.js
+++ server/app.js
@@ -26,8 +26,11 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/test", (req, res) => {
-  connection.query("SELECT * FROM deliveryInfo", (err, rows) => {
+app.get("/works", (req, res) => {
+  deliveryDate = req.query.deliveryDate
+  reciverAdd1 = req.query.reciverAdd1.map((v) => {return "'"+String(v)+"'"})
+  reciverAdd2 = req.query.reciverAdd2.map((v) => {return "'"+String(v)+"'"})
+  connection.query("SELECT * FROM deliveryInfo WHERE reciverAddr1 IN ("+reciverAdd1+") AND reciverAddr2 IN ("+reciverAdd2+")", (err, rows) => {
     if (err) {
       res.json({
         success: false,
@@ -39,6 +42,17 @@ app.get("/test", (req, res) => {
         rows,
       });
     }
+  });
+});
+
+
+app.get("/test", (req, res) => {
+  res.json({
+    0: ["35.46650", "129.24949"],
+    1: ["35.47768", "129.09151"],
+    2: ["35.36360", "129.10525"],
+    3: ["35.33226", "128.97749"],
+    4: ["35.22133", "128.86073"],
   });
 });
 
